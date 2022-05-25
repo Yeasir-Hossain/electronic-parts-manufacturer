4cab16dcf26087e307a66fcec28873e7d0a39daf
Yeasir-Hossain <yeasir06@gmail.com>
2022-05-25 15:09:37 +0600

    homepage sections added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,19 +4,23 @@ import Flip from 'react-reveal/Flip';
 import Fade from 'react-reveal/Fade';
 import Refund from './Refund';
 import Summary from './Summary';
+import Tools from './Tools';
+import Reviews from './Reviews';
 
 const Home = () => {
-    return (
-        <div>
-        <Flip left>
-          <Banner></Banner>
-        </Flip>
-        <Fade left>
+  return (
+    <div>
+      <Flip left>
+        <Banner></Banner>
+      </Flip>
+      <Fade left>
+        <Tools></Tools>
+        <Reviews></Reviews>
         <Refund></Refund>
         <Summary></Summary>
-        </Fade>
-      </div>
-    );
+      </Fade>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/home/Tools.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tools = () => {
+    return (
+        <div className='my-10'>
+            <h1 className='text-2xl text-center'>Tools</h1>
+        </div>
+    );
+};
+
+export default Tools;
